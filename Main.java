8927c162034085a3c153d3996038784fd94f697e
Steve Cordero <stevecords2008@gmail.com>
2021-01-23 02:38:20 +0000

    Added button2

--- Main.java
+++ Main.java
@@ -10,14 +10,14 @@ public class Main
     frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame1.setSize(500,500);
     
-
-
     JPanel panel1 = new JPanel(new GridLayout(2,2,20,20));
 
 
     JLabel label1 = new JLabel("Hello There");
     JTextField textfield1 = new JTextField(10);
 
+
+//---------------------Button1-------------------------------
     JButton button1 = new JButton("This is button1");
 
     button1.addActionListener(new ActionListener()
@@ -28,8 +28,26 @@ public class Main
       }//end block for function of Button1
     });//end the ActionListener for Button1
 
+//---------------------------Button2------------------------
+    JButton button2 = new JButton("This is button1");
+
+    button1.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        System.out.println("hello there buddy2");
+      }//end block for function of Button2
+    });//end the ActionListener for Button2
+
+//-----------------------Button3----------------------------
+
+
+
+
+
     panel1.add(button1);
+    panel1.add(button2);
     frame1.getContentPane().add(panel1);
     frame1.setVisible(true);
-  }
-}
+  }//end main method
+}//end class Main
