cad643146b77047e1ca7d765c861e8c908f6aa81
Steve Cordero <stevecords2008@gmail.com>
2021-01-23 02:28:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    JFrame frame1 = new JFrame("hello there");
+    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame1.setSize(500,500);
+    
+
+
+    JPanel panel1 = new JPanel(new GridLayout(2,2,20,20));
+
+
+    JLabel label1 = new JLabel("Hello There");
+    JTextField textfield1 = new JTextField(10);
+
+    JButton button1 = new JButton("This is button1");
+
+    button1.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        System.out.println("hello there buddy");
+      }//end block for function of Button1
+    });//end the ActionListener for Button1
+
+    panel1.add(button1);
+    frame1.getContentPane().add(panel1);
+    frame1.setVisible(true);
+  }
+}
